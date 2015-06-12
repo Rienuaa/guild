4fb9da9fdfeb947c5c1fad2ec928dcc71c02ded1
Rienuaa <aaacantor@gmail.com>
2015-06-12 13:58:08 -0400

    help
    augh

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var GPS = 0;
 var inf = 0;
 var IPS = 0;
 
-//name = [number, amount they produce per second, cost in gold, cost in inf, cost of previous unit, per second]
-var unit7 = [0, 8, 10000000, 1, 0]; //KINGDOMS
-var unit6 = [0, 7, 1000000, 1, 0]; //TERRITORIES
-var unit5 = [0, 6, 100000, 1, 0]; //LARGE HALLS
-var unit4 = [0, 5, 10000, 1, 0]; //GUILD HALLS
-var unit3 = [0, 4, 1000, 1, 0]; //GUILD HOUSES
-var unit2 = [0, 3, 100, 1, 0]; //PARTIES
-var unit1 = [0, 2, 10, 1, 0]; //ADVENTURERS
+//name = [number, amount they produce per second, cost in gold, cost in inf, cost of previous unit]
+var unit7 = [0, 8, 10000000, 1]; //KINGDOMS
+var unit6 = [0, 7, 1000000, 1]; //TERRITORIES
+var unit5 = [0, 6, 100000, 1]; //LARGE HALLS
+var unit4 = [0, 5, 10000, 1]; //GUILD HALLS
+var unit3 = [0, 4, 1000, 1]; //GUILD HOUSES
+var unit2 = [0, 3, 100, 1]; //PARTIES
+var unit1 = [0, 2, 10, 1]; //ADVENTURERS
 
 function goldUpdate(){
 	document.getElementById('GOLD').innerHTML = gold;
@@ -130,13 +130,13 @@ function buyUnit7(number){
 
 //THE LOOP OF THE GAME ITERATES THROUGH THIS ONCE PER SECOND
 function click(number){
-	unit7[0] = unit7[0] + (unit7[4] * number);
-	unit6[0] = unit6[0] + (unit6[4] * number);
-	unit5[0] = unit5[0] + (unit5[4] * number);
-	unit4[0] = unit4[0] + (unit4[4] * number);
-	unit3[0] = unit3[0] + (unit3[4] * number);
-	unit2[0] = unit2[0] + (unit2[4] * number);
-	unit1[0] = unit1[0] + (unit1[4] * number);
+	//unit7[0] = unit7[0] + (unit8[4] * number);
+	unit6[0] = unit6[0] + ((unit7[0] * unit7[1])* number);
+	unit5[0] = unit5[0] + ((unit6[0] * unit6[1])* number);
+	unit4[0] = unit4[0] + ((unit5[0] * unit5[1])* number);
+	unit3[0] = unit3[0] + ((unit4[0] * unit4[1])* number);
+	unit2[0] = unit2[0] + ((unit3[0] * unit3[1])* number);
+	unit1[0] = unit1[0] + ((unit2[0] * unit2[1])* number);
 	GPS = (unit1[1] * unit1[0]); //I know this is shitty coding practice, but it really helps me follow it
 	gold = gold + (GPS * number);
 	//inf = inf + (IPS * number);
